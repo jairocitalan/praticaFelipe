7f557dca883bc97257dc0635342b2852e3c1cfaa
jairoChavez <jairocitalan4@gmail.com>
2018-08-17 23:55:02 -0500

    second commit

--- src/main/java/com/java/mx/modelo/Administrativo.java
+++ src/main/java/com/java/mx/modelo/Administrativo.java
@@ -1,5 +1,21 @@
 package com.java.mx.modelo;
 
-public class Administrativo {
+public class Administrativo extends Persona{
+	private String cedula;
+	private String puesto;
+
+	
+	public Administrativo(String nombre, String apellidos, int edad, double peso, String cedula, String puesto) {
+		super(nombre, apellidos, edad, peso);
+		this.cedula = cedula;
+		this.puesto = puesto;
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void imprimeDatos() {
+		super.imprimeDatos();
+		System.out.println("Cedula: "+this.cedula.toUpperCase()+"puesto: "+this.puesto.toUpperCase());
+	}
 
 }
